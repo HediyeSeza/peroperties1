838a08941aa7703a19e3dcb17aedd707f44c68e3
sezahedi <sezahedi13@gmail.com>
2022-07-18 00:35:10 +0430

    Add project files.

---
+++ peroperties1/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peroperties1
+{
+    public class Employee
+    {
+        int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
---
+++ peroperties1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peroperties1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em=new Employee();
+            em.Id = 21;
+            Console.WriteLine("Your Employee id is : ");
+            Console.WriteLine(em.Id);
+            Console.ReadKey();
+        }
+    }
+}
